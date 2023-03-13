9defb89a16d10227da508f28926ef6d0e332df62
Jacek112 <jacekteodorczyk01@gmail.com>
2023-03-13 20:38:37 +0100

    Dodanie podziału na warstwy, zalążki interfejsu.

---
+++ Etap1/Dane/Kula.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dane
+{
+    public class Kula
+    {
+        private int height;
+        private int width;
+        private int x;
+        private int y;
+
+        public Kula(int height, int width, int x, int y)
+        {
+            this.height = height;
+            this.width = width;
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ Etap1/Logika/LogikaKuli.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dane;
+
+
+namespace Logika
+{
+    internal class LogikaKuli
+    {
+        Kula kula = new Kula(30, 30, 0, 0);
+        public void spawn()
+        {
+
+        }
+    }
+}
